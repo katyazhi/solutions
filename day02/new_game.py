49c424cc0bc575bb275891c8c8fcd1781f139cbc
Ekaterina Zhigileva <katya.zhigileva@mail.ru>
2024-04-29 19:56:05 +0300

    improved code

--- day02/new_game.py
+++ day02/new_game.py
@@ -1,17 +1,20 @@
 import random
 print("Hello! You will need to guess a randomly generated number from 1 to 20.")
-game = input("Are you ready for the new game? Type yes or no: ")
-while game.lower() == ("yes"):
-    answer = random.randint(1, 20)
-    number = None
-    count = 0
-    while answer != number:
-        number = int(input("Guess the number: "))
-        count += 1
-        if answer < number:
-            print("No, it is too big.")
-        if answer > number:
-            print("No, it is too small.")
-    print(f"You are right! It took you {count} attempts to win the game.")
+while True:
     game = input("Are you ready for the new game? Type yes or no: ")
-else: print("See you next time!")
+    if game.lower() == ("yes"):
+        answer = random.randint(1, 20)
+        count = 0
+        while True: 
+            number = int(input("Guess the number: "))
+            count += 1
+            if answer < number:
+                print("No, it is too big.")
+            if answer > number:
+                print("No, it is too small.")
+            if answer == number:
+                print(f"You are right! It took you {count} attempts to win the game.")
+                break
+    else:
+        print("See you next time!")
+        break
