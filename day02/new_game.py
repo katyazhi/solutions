45e6ef536728f6a3b392e840ee8ec71f45da2fbf
Ekaterina Zhigileva <katya.zhigileva@mail.ru>
2024-05-03 23:10:07 +0300

    black

--- day02/new_game.py
+++ day02/new_game.py
@@ -1,11 +1,12 @@
 import random
+
 print("Hello! You will need to guess a randomly generated number from 1 to 20.")
 while True:
     game = input("Are you ready for the new game? Type yes or no: ")
     if game.lower() == ("yes"):
         answer = random.randint(1, 20)
         count = 0
-        while True: 
+        while True:
             number = int(input("Guess the number: "))
             count += 1
             if answer < number:
